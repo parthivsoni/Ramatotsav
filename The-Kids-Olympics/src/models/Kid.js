077c0328dcaba7c558942a6dd02003ac2da28772
parthivsoni <parthiv20soni@gmail.com>
2026-02-07 17:01:33 +0530

    updated raipur - 2

--- The-Kids-Olympics/src/models/Kid.js
+++ The-Kids-Olympics/src/models/Kid.js
@@ -36,7 +36,7 @@
           "Sarangpur",
           "Raipur Sanskardham",
           "Raipur Sanskardham Shishu",
-          "Raipur 2",
+          "Raipur - 2",
           "New Entry"
         ],
       },
